5195d704eb1262b9fe73d6b976efbfb8cbbc7022
Yang Cao <yang.cao@liferay.com>
2023-09-20 20:14:42 +0800

    Simplify

--- liferay/apps/trigger_gauntlet.py
+++ liferay/apps/trigger_gauntlet.py
@@ -28,49 +28,7 @@ def create_gauntlet_ticket(target_branch):
 
     return jira_connection.create_issue(fields=issue_dict)
 
-def update_local_branch(repo, target_branch):
-    repo.remotes['upstream'].pull(target_branch)
-
-def clean_working_tree(repo):
-    repo.head.reset(working_tree=True)
-
-def checkout(local_branch, repo):
-    repo.heads[local_branch].checkout()
-
-def create_branch(branch_name, repo):
-    repo.create_head(branch_name)
-
-def make_changes_on_local_repo(commit_message, file_name, repo):
-    print("Making changes on local repo...")
-
-    open(file_name, "w").close()
-    repo.index.add([file_name])
-    repo.index.commit(commit_message)
-
-def create_gaunlet_pr(remote_repo, target_branch, testing_branch, ticket_number):
-    body= JIRA_INSTANCE + "/browse/" + ticket_number
-    head = get_credentials("GITHUB_USER_NAME") + ":" + testing_branch
-    title = ticket_number + ' | ' + target_branch
-
-    return remote_repo.create_pull(title=title,body=body,head=head,base=target_branch)
-
-def run_gauntlet(pr):
-    print("Run ci:test:gauntlet-bucket")
-
-    pr.create_issue_comment("ci:test:gauntlet-bucket")
-
-def paste_pr_to_ticket(gauntlet_ticket, pr):
-    gauntlet_ticket.update(description = pr.html_url)
-
-def main(legacy_repo_path, target_branch):
-    gauntlet_ticket = create_gauntlet_ticket(target_branch)
-
-    testing_branch = f'{target_branch}-qa-{gauntlet_ticket.key[-5:]}'
-
-    local_repo = Repo(legacy_repo_path)
-
-    print("Pulling from upstream...")
-
+def create_testing_branch(local_repo, target_branch):
     clean_working_tree(local_repo)
 
     checkout(target_branch, local_repo)
@@ -81,25 +39,56 @@ def main(legacy_repo_path, target_branch):
 
     update_local_branch(local_repo, target_branch)
 
-    print(f"Creating testing branch...")
-
     create_branch("temp_branch", local_repo)
 
     checkout("temp_branch", local_repo)
 
-    make_changes_on_local_repo(f'{gauntlet_ticket.key} TEMP for gauntlet testing', os.path.join(legacy_repo_path, "temp.text"), local_repo)
-
+def create_gaunlet_pr(local_repo, remote_branch, target_branch, ticket_number):
     g = get_github_connection()
 
     remote_repo = get_remote_repo(g, f'{get_credentials("GITHUB_USER_NAME")}/liferay-portal-ee')
 
-    push_branch_to_origin(local_repo, "temp_branch", testing_branch)
+    push_branch_to_origin(local_repo, "temp_branch", remote_branch)
 
-    pr = create_gaunlet_pr(remote_repo, target_branch, testing_branch, gauntlet_ticket.key)
+    body= JIRA_INSTANCE + "/browse/" + ticket_number
+    head = get_credentials("GITHUB_USER_NAME") + ":" + remote_branch
+    title = ticket_number + ' | ' + target_branch
+
+    return remote_repo.create_pull(title=title,body=body,head=head,base=target_branch)
+
+def make_changes_for_gauntlet_test(commit_message, file_name, repo):
+    print("Making changes for gauntlet test...")
+
+    open(file_name, "w").close()
+    repo.index.add([file_name])
+    repo.index.commit(commit_message)
+
+def paste_pr_to_ticket(gauntlet_ticket, pr):
+    gauntlet_ticket.update(description = pr.html_url)
+
+def run_gauntlet(pr):
+    print("Run ci:test:gauntlet-bucket")
+
+    pr.create_issue_comment("ci:test:gauntlet-bucket")
+
+def main(legacy_repo_path, target_branch):
+    gauntlet_ticket = create_gauntlet_ticket(target_branch)
+
+    remote_branch = f'{target_branch}-qa-{gauntlet_ticket.key[-5:]}'
+
+    local_repo = Repo(legacy_repo_path)
+
+    create_testing_branch(local_repo, target_branch)
+
+    make_changes_for_gauntlet_test(f'{gauntlet_ticket.key} TEMP for gauntlet testing', os.path.join(legacy_repo_path, "temp.text"), local_repo)
+
+    pr = create_gaunlet_pr(local_repo, remote_branch, target_branch, gauntlet_ticket.key)
 
     run_gauntlet(pr)
 
     paste_pr_to_ticket(gauntlet_ticket, pr)
+
+    print("Successful")
 
     print(JIRA_INSTANCE + "/browse/" + str(gauntlet_ticket))
 
