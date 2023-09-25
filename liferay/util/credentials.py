9754fa150b06033a6da57b5dcac0129847d47130
Yang Cao <yang.cao@liferay.com>
2023-09-25 14:24:42 +0800

    Catch credentials-ext nonexist when launch app at the first time

--- liferay/util/credentials.py
+++ liferay/util/credentials.py
@@ -7,14 +7,17 @@ from jproperties import Properties
 def get_credentials(key):
     configs = Properties()
 
-    with open(
-        os.path.join(os.path.dirname(__file__), "../../")
-        + "/credentials-ext.properties",
-        "rb",
-    ) as config_file:
-        configs.load(config_file)
+    try:
+        with open(
+            os.path.join(os.path.dirname(__file__), "../../")
+            + "/credentials-ext.properties",
+            "rb",
+        ) as config_file:
+            configs.load(config_file)
 
-    return configs.get(key).data
+        return configs.get(key).data
+    except:
+        pass
 
 
 def open_credentials():
