c1f159013a2a47d154c09ee92595996fb68859d5
Yang Cao <yang.cao@liferay.com>
2023-09-27 05:51:36 +0800

    Use textarea to allow users to define description and comments

--- liferay/apps/write_comments.py
+++ liferay/apps/write_comments.py
@@ -8,14 +8,12 @@ from liferay.jira.jira_constants import *
 from liferay.jira.jira_util import *
 
 
-def main(commit_id, description, env, ticket_number, type):
+def main(comments, ticket_number):
     jira_connection = get_jira_connection()
-
-    comment_template = generate_comment(commit_id, description, env, type)
 
     print("Writing comments...")
 
-    comment_id = jira_connection.add_comment(ticket_number, comment_template)
+    comment_id = jira_connection.add_comment(ticket_number, comments)
 
     print("Successful")
 
--- liferay/jira/jira_util.py
+++ liferay/jira/jira_util.py
@@ -13,77 +13,100 @@ def assign_to_me(assigned, jira_connection, issue):
         issue.update(fields={"assignee": {"accountId": jira_connection.current_user()}})
 
 
-def generate_comment(commit_id, description, env, type):
+def generate_comment(type):
     match type:
         case "PID":
-            return f"{{color:#36b37e}}*PASSED*{{color}} Manual Testing following the steps in the description.\n\n \
-                    Fixed on:\n \
-                    {env}. Portal master GIT ID: {commit_id}.\n\n \
-                    {description}"
+            return (
+                "{color:#36b37e}*PASSED*{color} Manual Testing following the steps in the description.\n\n"
+                + "Fixed on:\n"
+                + "Tomcat 9.0.80 + MySQL. Portal master GIT ID: .\n\n"
+            )
         case "FID":
-            return f"{{color:#ff5630}}*FAILED*{{color}} Manual Testing following the steps in the description.\n\n \
-                    Failed on:\n \
-                    {env}. Portal master GIT ID: {commit_id}. \n\n \
-                    {description}"
+            return (
+                "{color:#ff5630}*FAILED*{color} Manual Testing following the steps in the description.\n\n"
+                + "Failed on:\n"
+                + "Tomcat 9.0.80 + MySQL. Portal master GIT ID: .\n\n"
+            )
         case "NID":
-            return f"{{color:#36b37e}}*No Longer Reproducible*{{color}} through Manual Testing following the steps in the description.\n\n \
-                    No Longer Reproducible on:\n \
-                    {env}. Portal master GIT ID: {commit_id}.\n\n \
-                    {description}"
+            return (
+                "{color:#36b37e}*No Longer Reproducible*{color} through Manual Testing following the steps in the description.\n\n"
+                + "No Longer Reproducible on:\n"
+                + "Tomcat 9.0.80 + MySQL. Portal master GIT ID: .\n\n"
+            )
         case "PF":
-            return f"{{color:#36b37e}}*PASSED*{{color}} Manual Testing using the following steps:\n\n#  \n#  \n#  \n\n \
-                    Fixed on:\n \
-                    {env}. Portal master GIT ID: {commit_id}.\n\n \
-                    {description}"
+            return (
+                "{color:#36b37e}*PASSED*{color} Manual Testing using the following steps:\n"
+                + "# \n# \n# \n\n"
+                + "Fixed on:\n"
+                + "Tomcat 9.0.80 + MySQL. Portal master GIT ID: .\n\n"
+            )
         case "FF":
-            return f"{{color:#ff5630}}*FAILED*{{color}} Manual Testing using the following steps:\n\n#  \n#  \n#  \n\n \
-                    Failed on:\n \
-                    {env}. Portal master GIT ID: {commit_id}.\n\n \
-                    {description}"
+            return (
+                "{color:#ff5630}*FAILED*{color} Manual Testing using the following steps:\n"
+                + "# \n# \n# \n\n"
+                + "Failed on:\n"
+                + "Tomcat 9.0.80 + MySQL. Portal master GIT ID: .\n\n"
+            )
         case "NF":
-            return f"{{color:#36b37e}}*No Longer Reproducible*{{color}} through Manual Testing using the following steps:\n\n#  \n#  \n#  \n\n \
-                    No Longer Reproducible on:\n \
-                    {env}. Portal master GIT ID: {commit_id}.\n\n \
-                    {description}"
+            return (
+                "{color:#36b37e}*No Longer Reproducible*{color} through Manual Testing using the following steps:\n"
+                + "# \n# \n# \n\n"
+                + "No Longer Reproducible on:\n"
+                + "Tomcat 9.0.80 + MySQL. Portal master GIT ID: .\n\n"
+            )
         case "R":
-            return f"(!) Reproduced on:\n \
-                    {env}. Portal master GIT ID: {commit_id}."
+            return (
+                "(!) Reproduced on:\n"
+                + "Tomcat 9.0.80 + MySQL. Portal master GIT ID: ."
+            )
         case "RU":
-            return f"(!) Reproduced on:\n \
-                    Upgrade From: 7.4.13-DXP-U60.\n \
-                    {env}. Portal master GIT ID: {commit_id}."
+            return (
+                "(!) Reproduced on:\n"
+                + "Upgrade From: 7.4.13-DXP-U60.\n"
+                + "Tomcat 9.0.80 + MySQL. Portal master GIT ID: ."
+            )
         case "TV":
-            return f"*Case 1:* Passed.\n \
-                    *Case 2:* Passed.\n \
-                    *Case 3:* Passed.\n\n \
-                    *Tested on:*\n \
-                    {env}. Portal master GIT ID: {commit_id}."
+            return (
+                "*Case 1:* Passed.\n"
+                + "*Case 2:* Passed.\n"
+                + "*Case 3:* Passed.\n\n"
+                + "*Tested on:*\n"
+                + "Tomcat 9.0.80 + MySQL. Portal master GIT ID: ."
+            )
 
 
 def generate_description(type):
     match type:
         case "STR":
-            return "(on) *Steps to reproduce:*\n\n#  \n#  \n#  \n\n \
-                    (/) *Expected Results:*\n\n \
-                    (x) *Actual Results:*\n\n \
-                    (!) Reproduced on:\n \
-                    Tomcat 9.0.80 + MySQL. Portal master GIT ID: .\n\n \
-                    (/) Cannot be Reproduced on:\n \
-                    Tomcat 9.0.80 + MySQL. Portal 7.4.13-DXP-U94."
+            return (
+                "(on) *Steps to reproduce:*\n"
+                + "# \n# \n# \n# \n# \n\n"
+                + "(/) *Expected Results:*\n\n\n"
+                + "(x) *Actual Results:*\n\n\n"
+                + "(!) Reproduced on:\n"
+                + "Tomcat 9.0.80 + MySQL. Portal master GIT ID: .\n\n"
+                + "(/) Cannot be Reproduced on:\n"
+                + "Tomcat 9.0.80 + MySQL. Portal 7.4.13-DXP-U94."
+            )
         case "TC":
-            return "h3. *Test Cases*\n\n \
-                    *Case 1:*\n \
-                    The user can login after entering correct username and password.\n \
-                    Test Strategy: CRITICAL\n \
-                    Can be covered by POSHI?: Yes\n\n#  \n#  \n# \n\n \
-                    *Case 2:*\n \
-                    The user cannot login after entering wrong username and password.\n \
-                    Test Strategy: HIGH\n \
-                    Can be covered by POSHI?: Yes\n\n#  \n#  \n# \n\n \
-                    *Case 3:*\n \
-                    The user cannot login without entering password.\n \
-                    Test Strategy: MEDIUM\n \
-                    Can be covered by POSHI?: Yes\n\n#  \n#  \n# "
+            return (
+                "h3.*Test Cases*\n"
+                + "*Case 1:*\n"
+                + "The user can login after entering correct username and password.\n"
+                + "Test Strategy: CRITICAL\n"
+                + "Can be covered by POSHI?: Yes\n"
+                + "# \n# \n# \n\n"
+                + "*Case 2:*\n"
+                + "The user cannot login after entering wrong username and password.\n"
+                + "Test Strategy: HIGH\n"
+                + "Can be covered by POSHI?: Yes\n"
+                + "# \n# \n# \n\n"
+                + "*Case 3:*\n"
+                + "The user cannot login without entering password.\n"
+                + "Test Strategy: MEDIUM\n"
+                + "Can be covered by POSHI?: Yes\n"
+                + "# \n# \n# "
+            )
 
 
 def get_project_components(jira_connection, project_key):
