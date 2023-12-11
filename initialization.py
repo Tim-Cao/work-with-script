4068ecf5ae69673dcca2e4a276b631b9ba805f14
Yang Cao <yang.cao@liferay.com>
2023-12-12 05:12:52 +0800

    Install the latest dependencies during initialization

--- initialization.py
+++ initialization.py
@@ -40,7 +40,37 @@ def generate_credentials():
         print("credentials-ext.properties is created")
 
 
+def upgrade_modules():
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "--upgrade", "jproperties"]
+    )
+
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "--upgrade", "pandas"]
+    )
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "jira"])
+
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "--upgrade", "jsonpath-ng"]
+    )
+
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "--upgrade", "PyGithub"]
+    )
+
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "--upgrade", "GitPython"]
+    )
+
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "--upgrade", "textual"]
+    )
+
+
 if __name__ == "__main__":
     install_modules()
+
+    upgrade_modules()
 
     generate_credentials()
