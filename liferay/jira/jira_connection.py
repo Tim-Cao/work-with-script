fa602f0321e3e5cf3f88e9f987a1819c388e5d45
Yang Cao <yang.cao@liferay.com>
2023-12-20 13:58:46 +0800

    Allow all content types

--- liferay/jira/jira_connection.py
+++ liferay/jira/jira_connection.py
@@ -14,5 +14,6 @@ def get_jira_connection():
 
     return JIRA(
         JIRA_INSTANCE,
+        options={"headers": {"Accept": "*/*"}},
         basic_auth=(get_credentials("JIRA_USER_NAME"), get_credentials("JIRA_TOKEN")),
     )
