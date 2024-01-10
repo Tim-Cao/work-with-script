cdd53cec0320bd92762b23a744f421f520b3801c
Yang Cao <yang.cao@liferay.com>
2024-01-11 06:05:48 +0800

    Add more widgets

--- widgets/textarea_field.py
+++ widgets/textarea_field.py
@@ -11,7 +11,7 @@ root = os.path.join(os.path.dirname(__file__), "../")
 sys.path.append(root)
 
 
-class TextField(Vertical):
+class TextAreaField(Vertical):
     CSS_PATH = root + "/liferay/src/css/main.css"
 
     def __init__(
@@ -19,15 +19,13 @@ class TextField(Vertical):
         field_label: Optional[str] = None,
         input_id: Optional[str] = None,
         text_classes: Optional[str] = None,
-        value: Optional[str] = None,
     ) -> None:
         super().__init__()
         self.field_label = field_label
         self.field_id = input_id + "-label"
         self.input_id = input_id
         self.text_classes = text_classes
-        self.value = value
 
     def compose(self) -> ComposeResult:
         yield Label(self.field_label, id=self.field_id, classes=self.text_classes)
-        yield Input(id=self.input_id, classes=self.text_classes, value=self.value)
+        yield TextArea(id=self.input_id, classes=self.text_classes)
