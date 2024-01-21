cbb575bdf70041128a0ee8af983a0ef574b98098
Yang Cao <yang.cao@liferay.com>
2024-01-21 08:17:49 +0800

    Add pyperclip to dependencies

--- upgrade.py
+++ upgrade.py
@@ -1,22 +1,16 @@
 import subprocess
 import sys
 
-subprocess.check_call(
-    [sys.executable, "-m", "pip", "install", "--upgrade", "jproperties"]
-)
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pandas"])
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "jira"])
-
-subprocess.check_call(
-    [sys.executable, "-m", "pip", "install", "--upgrade", "jsonpath-ng"]
-)
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "PyGithub"])
-
-subprocess.check_call(
-    [sys.executable, "-m", "pip", "install", "--upgrade", "GitPython"]
-)
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "textual"])
+for package in [
+    "jproperties",
+    "pandas",
+    "jira",
+    "jsonpath-ng",
+    "PyGithub",
+    "pyperclip",
+    "GitPython",
+    "textual",
+]:
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "--upgrade", package]
+    )
